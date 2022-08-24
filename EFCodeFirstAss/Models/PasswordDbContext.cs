8bed7761708c004062ccd5fe42b0a3a8edbfac69
Abayomi-Azeem <abayomioyaifo@gmail.com>
2022-08-24 08:06:23 +0100

    Add project files.

---
+++ EFCodeFirstAss/Models/PasswordDbContext.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EFCodeFirstAss.Models
+{
+    public class PasswordDbContext: DbContext
+    {
+        public PasswordDbContext()
+        {
+
+        }
+        public PasswordDbContext(DbContextOptions<PasswordDbContext> options): base(options)
+        {
+
+        }
+
+        public DbSet<Passwords> Passwords { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer
+                    ("Server=.;Database=Passwordmanagerdb;Trusted_Connection=True; Encrypt=True;TrustServerCertificate=True;");
+            }
+        }
+    }
+}
